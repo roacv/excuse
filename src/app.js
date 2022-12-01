fcf0f1f157bcf41ec11b6fe79a23ed3c04d2953d
roacv <roacv@hotmail.com>
2022-12-01 15:25:48 +0000

    cambio datos en duro

--- src/app.js
+++ src/app.js
@@ -16,11 +16,9 @@ window.onload = function() {
     "during my lunch",
     "while I was praying"
   ];
-  let min = 0;
-  let max = 3;
-  let rnd1 = Math.floor(Math.random() * (3 - 0 + 1) + 0);
-  let rnd2 = Math.floor(Math.random() * (3 - 0 + 1) + 0);
-  let rnd3 = Math.floor(Math.random() * (4 - 0 + 1) + 0);
+  let rnd1 = Math.floor(Math.random() * who.length - 2);
+  let rnd2 = Math.floor(Math.random() * 4);
+  let rnd3 = Math.floor(Math.random() * 5);
   excuse.innerHTML = who[rnd1] + " " + what[rnd2] + " " + when[rnd3];
   //console.log("Hello Rigo from the console!");
 };
